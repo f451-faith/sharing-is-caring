63b949a6846c5e0363f2137f498ddfc676dd0886
Quentin Creuzet <quentin@f451.faith>
2021-12-09 14:32:26 +0100

    New functions

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ LIST OF FUNCTIONS
 */
 
 function getAllBooks(req, res) {
-  if (Object.keys(req.query).length) {
+  if (Object.keys(req.query).length > 0) {
     // Check if there's anything on the parameters
     const filteredData = getFilteredData(data, req.query);
     res.send(filteredData);
@@ -38,24 +38,46 @@ function getAllBooks(req, res) {
 function getFilteredData(data, parameters) {
   Object.entries(parameters).forEach(([key, value]) => {
     if (key == "q") data = filterDataByQ(data, value);
-    if (key == "author") data = filterDataByAuthor(data, value);
+    if (key == "editor") data = filterDataByEditor(data, value);
     if (key == "min_year") data = filterDataByMinYear(data, value);
+    if (key == "max_year") data = filterDataByMaxYear(data, value);
   });
   return data;
 }
 
 function filterDataByQ(data, value) {
-  data = data.filter((book) => book.title.toLowerCase().includes(value.toLowerCase()));
+  data = data.filter(function (book) {
+    if (book.title) {
+      return book.title.toLowerCase().includes(value.toLowerCase());
+    }
+  });
   return data;
 }
 
-function filterDataByAuthor(data, value) {
-  // Filter the data with the value
+function filterDataByEditor(data, value) {
+  data = data.filter(function (book) {
+    if (book.editor) {
+      return book.editor.toLowerCase().includes(value.toLowerCase());
+    }
+  });
   return data;
 }
 
 function filterDataByMinYear(data, value) {
-  // Filter the data with the value
+  data = data.filter(function (book) {
+    if (book.year) {
+      return book.year >= value;
+    }
+  });
+  return data;
+}
+
+function filterDataByMaxYear(data, value) {
+  data = data.filter(function (book) {
+    if (book.year) {
+      return book.year <= value;
+    }
+  });
   return data;
 }
 
