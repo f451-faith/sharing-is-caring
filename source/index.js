45a8bf7b1f8ebefdd0c2a7e0c0d8f8df7f9efd8a
Quentin Creuzet <quentin@f451.faith>
2021-12-09 09:38:07 +0100

    Add code source

---
+++ source/index.js
@@ -0,0 +1,35 @@
+/*
+GENERAL SETUP
+*/
+
+const fs = require("fs");
+const dataFromJSON = JSON.parse(fs.readFileSync("../book.json"));
+let data = dataFromJSON;
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.listen(port, () => console.log("🏗️  Server available at http://localhost:3000"));
+
+/*
+LIST OF URLs
+*/
+
+app.get("/", (req, res) => res.send("Nothing to show 🚧"));
+app.get("/books", getAllBooks);
+app.get("/books/:book/", getSpecificBook);
+
+/*
+LIST OF FUNCTIONS
+*/
+
+function getAllBooks(req, res) {
+  res.send(data);
+}
+
+function getSpecificBook(req, res) {
+  const id = req.params.book;
+  const item = data.find((i) => i.id == id);
+  res.send(item);
+}
