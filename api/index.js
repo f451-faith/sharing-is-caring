97c7c95dda50e62e20da3077828105358780da5b
Quentin Creuzet <quentin@f451.faith>
2021-12-09 10:06:16 +0100

    Add nodemon

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.listen(port, () => console.log("🏗️  Server available at http://localhost:3000"));
+app.listen(port, () => console.log("Server available at http://localhost:3000 🏗️"));
 
 /*
 LIST OF URLs
