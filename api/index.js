4350d7b2e0270f6dc59a7e7555135dfa0188e1dc
Quentin Creuzet <quentin@f451.faith>
2021-12-10 09:54:06 +0100

    Remove city function

--- api/index.js
+++ api/index.js
@@ -27,7 +27,6 @@ LIST OF FUNCTIONS
 
 function getAllBooks(req, res) {
   if (Object.keys(req.query).length > 0) {
-    // Check if there's anything on the parameters
     const filteredData = getFilteredData(data, req.query);
     res.send(filteredData);
   } else {
@@ -38,7 +37,6 @@ function getAllBooks(req, res) {
 function getFilteredData(data, parameters) {
   Object.entries(parameters).forEach(([key, value]) => {
     if (key == "q") data = filterDataByQ(data, value);
-    // if (key == "city") data = filterDataByCity(data, value);
     if (key == "editor") data = filterDataByEditor(data, value);
     if (key == "min_year") data = filterDataByMinYear(data, value);
     if (key == "max_year") data = filterDataByMaxYear(data, value);
@@ -79,38 +77,6 @@ function filterDataByQ(data, value) {
   });
   return data;
 }
-
-/*
-function filterDataByCity(data, value) {
-  data = data.filter(function (book) {
-    // Check if the book has an ISBN
-    if (book.isbn) {
-      // Create the URL for the Google API where the info of the book are
-      var url = "https://openlibrary.org/isbn/" + book.isbn + ".json";
-      // Fetch the data from this URL
-      fetch(url)
-        .then(function (response) {
-          // We transform the data to JSON
-          if (response.status != 404) return response.json();
-        })
-        .then(function (json) {
-          if (json) {
-            var cities = json["publish_places"];
-            // We check if there's any publish places
-            if (cities) {
-              // Transform array of publish places into a string
-              var citiesString = cities.join(", ");
-              // Check if the author's string includes the value
-              return citiesString.toLowerCase().includes(value.toLowerCase());
-            }
-          }
-        });
-    }
-  });
-
-  return data;
-}
-*/
 
 function filterDataByEditor(data, value) {
   data = data.filter(function (book) {
