4f171d7cc98508fee8347325fc144f9bf6dff59b
Quentin Creuzet <quentin@f451.faith>
2021-12-10 14:39:13 +0100

    CORS Policy

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
+  next();
+});
+
 app.listen(port, () => console.log("Server available at http://localhost:3000 🏗️"));
 
 /*
